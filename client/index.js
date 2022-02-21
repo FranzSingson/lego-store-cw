4d061cd62a1984e84d4233d7fc1b427090918d57
UP2011325 <74835413+franz-up2011325@users.noreply.github.com>
2022-02-21 23:02:59 +0000

    Linter setup

--- client/index.js
+++ client/index.js
@@ -1,36 +1,36 @@
 function makeGrid() {
-    const maxProducts = 50;
-    for (let i = 1; i < maxProducts; i++) {
-        const newSection = document.createElement('div')
-        newSection.id = `product${i}`;
-        newSection.className = 'allBoxes-class'
+  const maxProducts = 50;
+  for (let i = 1; i < maxProducts; i++) {
+    const newSection = document.createElement('div');
+    newSection.id = `product${i}`;
+    newSection.className = 'allBoxes-class';
 
-        const newPara = document.createElement('p')
-        // newPara.textContent = 'Div box'
+    const newPara = document.createElement('p');
+    // newPara.textContent = 'Div box'
 
-        newSection.appendChild(newPara);
+    newSection.appendChild(newPara);
 
-        const elemMain = document.getElementById('main-board')
-        elemMain.appendChild(newSection);
-        console.log("MakeGrid is working")
-    }
+    const elemMain = document.getElementById('main-board');
+    elemMain.appendChild(newSection);
+    console.log('MakeGrid is working');
+  }
 }
 
 function divideBoxes() {
-    const allBoxes = document.querySelectorAll('.allBoxes-class');
-    for (const eachBox of allBoxes) {
-        const picDiv = document.createElement('div');
-        picDiv.className = `picDiv-class`;
-        picDiv.textContent = 'Product image goes here';
-        eachBox.append(picDiv);
-    }
+  const allBoxes = document.querySelectorAll('.allBoxes-class');
+  for (const eachBox of allBoxes) {
+    const picDiv = document.createElement('div');
+    picDiv.className = 'picDiv-class';
+    picDiv.textContent = 'Product image goes here';
+    eachBox.append(picDiv);
+  }
 
-    for (const eachBox of allBoxes) {
-        const picDesc = document.createElement('div');
-        picDesc.className = `picDesc-class`;
-        picDesc.textContent = 'Product desc goes here';
-        eachBox.append(picDesc);
-    }
+  for (const eachBox of allBoxes) {
+    var picDesc = document.createElement('div');
+    picDesc.className = 'picDesc-class';
+    picDesc.textContent = 'Product desc goes here';
+    eachBox.append(picDesc);
+  }
 }
 
 makeGrid();
