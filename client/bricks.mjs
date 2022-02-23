bc7fd164b6a280ec1cdd5b9751d943adfa19db74
UP2011325 <74835413+franz-up2011325@users.noreply.github.com>
2022-02-23 14:49:24 +0000

    Added test bricks and insertion of product

---
+++ client/bricks.mjs
@@ -0,0 +1,16 @@
+export const bricks = [
+  { id: '300101/3001', name: 'Brick1', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/300101.jpg', colour: 'White' },
+  { id: '4211201/3001', name: 'Brick2', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/4211201.jpg', colour: 'Brown' },
+  { id: '4625629/3001', name: 'Brick3', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/4625629.jpg', colour: 'Blue - Medium Azure' },
+  { id: '4114319/3001', name: 'Brick4', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/4114319.jpg', colour: 'Brown - Brick Yellow' },
+  { id: '300124/3001', name: 'Brick5', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/300124.jpg', colour: 'Yellow - Bright Yellow' },
+  { id: '300123/3001', name: 'Brick6', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/300123.jpg', colour: 'Blue - Bright Blue' },
+  { id: '6117418/3001', name: 'Brick7', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/6117418.jpg', colour: 'Red - Dark Red' },
+  { id: '4205058/3001', name: 'Brick8', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/4205058.jpg', colour: 'Blue - Medium Blue' },
+  { id: '6075626/3001', name: 'Brick9', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/6075626.jpg', colour: 'Green - Sand Green' },
+  { id: '6135191/3001', name: 'Brick10', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/6135191.jpg', colour: 'Brown - Medium Nougat' },
+  { id: '6275133/3001', name: 'Brick11', price: '£0.20', imgSrc: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/6275133.jpg', colour: 'Blue - Earth Blue' },
+
+];
+
+// console.log(bricks[1].id)
--- client/index.js
+++ client/index.js
@@ -1,14 +1,18 @@
+import { bricks } from './bricks.mjs';
+
+const maxProducts = 50;
+let bricksStock = 0;
+
+
 function makeGrid() {
-  const maxProducts = 50;
   for (let i = 1; i < maxProducts; i++) {
     const newSection = document.createElement('div');
     newSection.id = `product${i}`;
     newSection.className = 'allBoxes-class';
 
-    const newPara = document.createElement('p');
+    // const newPara = document.createElement('p');
     // newPara.textContent = 'Div box'
-
-    newSection.appendChild(newPara);
+    // newSection.appendChild(newPara);
 
     const elemMain = document.getElementById('main-board');
     elemMain.appendChild(newSection);
@@ -17,21 +21,52 @@ function makeGrid() {
 }
 
 function divideBoxes() {
+  let count = 1;
   const allBoxes = document.querySelectorAll('.allBoxes-class');
   for (const eachBox of allBoxes) {
     const picDiv = document.createElement('div');
     picDiv.className = 'picDiv-class';
+    picDiv.id = `picDiv-id${count}`;
+    count++;
     picDiv.textContent = 'Product image goes here';
     eachBox.append(picDiv);
   }
 
   for (const eachBox of allBoxes) {
-    var picDesc = document.createElement('div');
-    picDesc.className = 'picDesc-class';
+    const picDesc = document.createElement('div');
+    picDesc.className = 'descDiv-class';
+
+    // const newPara = document.createElement("p"); uncomment this
+    // newPara.textContent = 'Product desc goes here'; uncomment this
+
+    // picDesc.append(newPara); uncomment this
     picDesc.textContent = 'Product desc goes here';
     eachBox.append(picDesc);
   }
 }
 
+function insertProduct() {
+  const allBoxes = document.querySelectorAll('.allBoxes-class');
+  for (const allBox of allBoxes) {
+    // insert image on each picDiv-class
+    const picDiv = allBox.querySelector('.picDiv-class');
+    const imgElem = document.createElement('img');
+    imgElem.src = bricks[bricksStock].imgSrc;
+    picDiv.appendChild(imgElem);
+    imgElem.setAttribute('style', 'height: 75%;');
+
+    const descDiv = allBox.querySelector('.descDiv-class');
+    const paraElem = document.createElement('p');
+    paraElem.textContent = `${'ID: ' + bricks[bricksStock].id + ' Colour: ' + bricks[bricksStock].colour + ' Price: ' + bricks[bricksStock].price}`;
+
+    descDiv.append(paraElem);
+    bricksStock++;
+  }
+}
+
+
 makeGrid();
 divideBoxes();
+insertProduct();
+
+// loop through the class of allBoxes-class
