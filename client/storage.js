72ac4efed82c977deabb54c33bfb7be7feb8330c
UP2011325 <74835413+franz-up2011325@users.noreply.github.com>
2022-03-28 21:44:45 +0100

    + and - working but needs page to refresh

--- client/storage.js
+++ client/storage.js
@@ -2,33 +2,37 @@ import { bricks } from './bricks.mjs';
 
 const carts = document.querySelectorAll('.addToBasketButton-class');
 
-
-for (let i = 0; i < carts.length; i++) {
-  carts[i].addEventListener('click', () => {
-    // console.log(bricks[i]);
-    cartNumbers(bricks[i]);
-    totalCost(bricks[i]);
-  });
+function init1() {
+  for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+      // console.log(bricks[i]);
+      cartNumbers(bricks[i]);
+      totalCost(bricks[i]);
+    });
+  }
 }
 
-function onLoadCartNumbers() {
-  const productNumbers = localStorage.getItem('cartNumbers');
-
+function updateBasketNum() {
+  const productNumbers = localStorage.getItem('cartQty');
   if (productNumbers) {
     document.querySelector('.basket span').textContent = productNumbers;
   }
 }
 
 function cartNumbers(product) {
-  let productNumbers = localStorage.getItem('cartNumbers');
+  let productNumbers = localStorage.getItem('cartQty');
 
   productNumbers = parseInt(productNumbers);
+  console.log(productNumbers);
 
+  // If productNumbers exist
   if (productNumbers) {
-    localStorage.setItem('cartNumbers', productNumbers + 1);
+    localStorage.setItem('cartQty', productNumbers + 1);
     document.querySelector('.basket span').textContent = productNumbers + 1;
-  } else {
-    localStorage.setItem('cartNumbers', 1);
+  }
+  // If there is products beforehand
+  else {
+    localStorage.setItem('cartQty', 1);
     document.querySelector('.basket span').textContent = 1;
   }
   setItems(product);
@@ -37,7 +41,6 @@ function cartNumbers(product) {
 function setItems(product) {
   let cartItems = localStorage.getItem('productsInCart');
   cartItems = JSON.parse(cartItems);
-  //   console.log('my cartItems are', cartItems);
 
   if (cartItems != null) {
     if (cartItems[product.name] === undefined) {
@@ -55,18 +58,19 @@ function setItems(product) {
     };
   }
   localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+  // plusBtn(product)
+  console.log('my cartItems are', cartItems);
 }
-
 
 function totalCost(product) {
   let cartCost = localStorage.getItem('totalCost');
 
   //   Use parseFloat instead of parseInt as we have decimal.
-  console.log('My CartCost is ', cartCost);
   console.log(typeof cartCost);
 
   if (cartCost != null) {
     cartCost = parseFloat(cartCost);
+    console.log('My CartCost is ', cartCost);
     localStorage.setItem('totalCost', cartCost + product.price);
   } else {
     localStorage.setItem('totalCost', product.price);
@@ -74,7 +78,99 @@ function totalCost(product) {
 }
 
 
-function showBasket() {
+// createBasketContent has to be here
+createBasketContent();
+// plusBtn();
+// init1();
+
+
+function quantityButtons() {
+  // Plus button
+  const plusProducts = document.querySelectorAll('.plus-btn');
+  for (let i = 0; i < plusProducts.length; i++) {
+    plusProducts[i].addEventListener('click', () => {
+      // console.log(bricks[i]);
+      cartNumbers(bricks[i]);
+      totalCost(bricks[i]);
+    });
+  }
+
+  // Subtract buttton#
+  const subtractProducts = document.querySelectorAll('.subtract-btn');
+  for (let i = 0; i < subtractProducts.length; i++) {
+    subtractProducts[i].addEventListener('click', () => {
+      // console.log(bricks[i]);
+      cartNumbers2(bricks[i]);
+      totalCost2(bricks[i]);
+    });
+  }
+}
+
+function cartNumbers2(product) {
+  let productNumbers = localStorage.getItem('cartQty');
+
+  productNumbers = parseInt(productNumbers);
+  console.log(productNumbers);
+
+  // If productNumbers exist
+  if (productNumbers) {
+    localStorage.setItem('cartQty', productNumbers - 1);
+    document.querySelector('.basket span').textContent = productNumbers - 1;
+  }
+  // If there is products beforehand
+  else {
+    localStorage.setItem('cartQty', 1);
+    document.querySelector('.basket span').textContent = 1;
+  }
+  setItems2(product);
+}
+
+function setItems2(product) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.name] === undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.name]: product,
+      };
+    }
+    cartItems[product.name].inCart -= 1;
+  } else {
+    product.inCart = 1;
+
+    cartItems = {
+      [product.name]: product,
+    };
+  }
+  localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+  // plusBtn(product)
+  console.log('my cartItems are', cartItems);
+}
+
+function totalCost2(product) {
+  let cartCost = localStorage.getItem('totalCost');
+
+  //   Use parseFloat instead of parseInt as we have decimal.
+  console.log(typeof cartCost);
+
+  if (cartCost != null) {
+    cartCost = parseFloat(cartCost);
+    console.log('My CartCost is ', cartCost);
+    localStorage.setItem('totalCost', cartCost - product.price);
+  } else {
+    localStorage.setItem('totalCost', product.price);
+  }
+}
+
+/* Notes
+Atm, + and - button works but needs the page to be refreshed to update the createBasketContent page
+When Quantity is 1 and negative button is pressed, it should remove the prodcut from the page. Therefore update local storage.
+*/
+
+
+function createBasketContent() {
   let basketItems = localStorage.getItem('productsInCart');
   basketItems = JSON.parse(basketItems);
   const productContainer = document.querySelector('.products-container');
@@ -89,21 +185,23 @@ function showBasket() {
     `;
     Object.values(basketItems).map(item => {
       productContainer.innerHTML += `
-        <div class="product">
+        <div class="product" id="${item.name}" >
         <span>${item.name} for £${item.price}</span>
         <img src="${item.imgSrc}">
-        <button type="button">Add one</button>
+        <button class="subtract-btn" type="button">-</button>
         <span>Quantity: ${item.inCart}</span>
-        <button type="button">Delete one</button>
-        <button type="button">Remove Product</button>
+        <button class="plus-btn" type="button">+</button>
+        <button id="remove-button" type="button">Remove Product</button>
         <span>£${item.inCart * item.price}</span>
         </div>
 
         `;
     });
   }
+  // removeObject(basketItems);
+  // console.log(bask)
 }
 
-
-onLoadCartNumbers();
-showBasket();
+init1();
+updateBasketNum();
+quantityButtons();
